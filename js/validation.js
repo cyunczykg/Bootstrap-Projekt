d4e2e656439fb0f5f62381cc83f831843cb426ce
Grzegorz Cyuńczyk <cyunczykg@gmail.com>
2023-05-25 10:09:33 +0200

    added JS mailto function, form desc edit.

--- js/validation.js
+++ js/validation.js
@@ -9,21 +9,21 @@ function validateForm() {
 	var email = document.getElementById('email').value;
 	if (email == '') {
 		document.querySelector('.status').innerHTML =
-			'Pole Twój e-mail nie może być puste';
+			'Pole E-mail kontaktowy nie może być puste';
 		return false;
 	} else {
 		var re =
 			/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 		if (!re.test(email)) {
 			document.querySelector('.status').innerHTML =
-				'Nieprawidłowy format adresu e-mail';
+				'Nieprawidłowy format kontaktowego adresu e-mail';
 			return false;
 		}
 	}
 	var subject = document.getElementById('subject').value;
 	if (subject == '') {
 		document.querySelector('.status').innerHTML =
-			'Pole temat wiadomości nie może być puste';
+			'Temat wiadomości nie może być pusty';
 		return false;
 	}
 	var message = document.getElementById('message').value;
@@ -33,4 +33,15 @@ function validateForm() {
 		return false;
 	}
 	document.querySelector('.status').innerHTML = 'Wysyłam...';
+	var link =
+		'mailto:someone@example.com?cc=' +
+		email +
+		'&' +
+		'subject=' +
+		subject +
+		'&body=' +
+		message +
+		'%0D%0A%0D%0A' +
+		name;
+	window.location.href = link;
 }
